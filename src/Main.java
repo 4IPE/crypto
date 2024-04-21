f14cfe9c786ba6f02033731c6737d34c0e631257
Daniil <99344227+4IPE@users.noreply.github.com>
2024-04-21 20:29:58 +0300

    add double encode

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         DoubleEncoder doubleEncoder = new DoubleEncoder();
         String str = doubleEncoder.encoder("двойная перестановка",new int[]{3,1,4,2},new int[]{3,2,4,1,5});
+        System.out.println(str);
         doubleEncoder.decoder(str,new int[]{3,1,4,2},new int[]{3,2,4,1,5});
     }
 }
